3cabdadf63a4e6388ca99341b5f5abe2a9f1d5b0
Klelia <kleliavoulgaraki@gmail.com>
2021-05-12 17:18:12 +0300

    Allow registration of patients with same password, but not same email

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -6,16 +6,17 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+// Changed in order to allow same passwords but not same emails
 @Data
 @MappedSuperclass
 public class User {
     @Column(unique = true)
     private String username;
     @Column(unique = true)
-    private String password;
+    private String email;
 
     private String name;
-    private String email;
+    private String password;
     private String address;
 
 
