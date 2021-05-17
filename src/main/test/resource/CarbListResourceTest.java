a63601132cd0f43182f110ba063c8526eec4f64d
Klelia <kleliavoulgaraki@gmail.com>
2021-05-17 13:22:58 +0300

    TODO Test

---
+++ src/main/test/resource/CarbListResourceTest.java
@@ -0,0 +1,14 @@
+package resource;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarbListResourceTest {
+    private CarbListResource carbListResource;
+    @BeforeEach
+    void init(){
+        carbListResource = new CarbListResource();
+    }
+
+}
